3ae5c8398e2772df562dd353112d069a95a768a4
Cambriel <yamato12367@gmail.com>
2024-04-07 11:33:13 +0200

    Added comments to getFunctions

--- utility/getFunctions.js
+++ utility/getFunctions.js
@@ -1,10 +1,18 @@
 import fs from 'fs';
 import path from 'path';
 
+/**
+ * Returns a list of all the exports from a directory
+ *  
+ * @param {string} folderPath is the directory in which the .js files are located
+ * @returns {Promise<BotCommand[]>} a list of all the exports from .js files within the given directory
+ */
 const getFunctions = async (folderPath) => {
     const files = fs.readdirSync(folderPath);
 
     const functionsList = [];
+
+    //Go through all the files that end in .js and get their exports
     for(const file of files){
         const filePath = path.join(filePath, file);
         
