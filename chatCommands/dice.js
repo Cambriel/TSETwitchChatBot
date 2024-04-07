3dd98ca9bc07e824e79331e68c612b516d6aee9f
Cambriel <yamato12367@gmail.com>
2024-04-07 12:40:06 +0200

    changed export type in dice.js

--- chatCommands/dice.js
+++ chatCommands/dice.js
@@ -1,11 +1,9 @@
 import { createBotCommand } from '@twurple/easy-bot';
 
-const dice = createBotCommand(
+export const dice = createBotCommand(
     'dice',
     (params, {reply}) => {
       const diceRoll = Math.floor(Math.random() * 6) + 1; 
       reply(`You rolled a ${diceRoll}`);
     }
 );
-
-module.exports = dice;
