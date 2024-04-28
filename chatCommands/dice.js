5abffc824d4b9027039dceea282a1f2400fe8d00
Cambriel <yamato12367@gmail.com>
2024-04-28 21:10:04 +0200

    Update: Dice, take first given parameter in command and default to 6

--- chatCommands/dice.js
+++ chatCommands/dice.js
@@ -1,11 +1,13 @@
-import { createBotCommand } from '@twurple/easy-bot';
+import { createBotCommand } from "@twurple/easy-bot";
 
-const command = createBotCommand(
-    'dice',
-    (params, {reply}) => {
-      const diceRoll = Math.floor(Math.random() * 6) + 1; 
-      reply(`You rolled a ${diceRoll}`);
-    }
-);
+const command = createBotCommand("dice", (params, { reply }) => {
+  let dAmount = 6;
+  if (params && Number(params[0]) != NaN) {
+    dAmount = Number(params[0]);
+  }
+
+  const diceRoll = Math.floor(Math.random() * dAmount) + 1;
+  reply(`You rolled a ${diceRoll}`);
+});
 
 export default command;
